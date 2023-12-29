4fec179b6829efc64fe281cdb1d317482ab8a23e
lnit <6626484+lnit@users.noreply.github.com>
2023-12-29 18:49:25 +0900

    has_many関連付けと初期値を追加

--- app/models/point_ranking.rb
+++ app/models/point_ranking.rb
@@ -1,2 +1,3 @@
 class PointRanking < Ranking
+  has_many :rank_items, class_name: 'PointRankItem', foreign_key: :ranking_id
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ApplicationRecord
+  has_many :rankings, dependent: :destroy
+
+  before_create do
+    self.secret = SecureRandom.urlsafe_base64
+  end
 end
