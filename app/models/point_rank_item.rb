fa6a249230e2c2736b92e130ee763c73a117e950
lnit <6626484+lnit@users.noreply.github.com>
2024-09-19 13:15:07 +0900

    スコア更新時に名前が消されてしまう問題を修正

--- app/models/point_rank_item.rb
+++ app/models/point_rank_item.rb
@@ -1,6 +1,8 @@
 class PointRankItem < ApplicationRecord
   belongs_to :ranking_board
 
+  attribute :name, :string,  default: "No Name"
+
   def name=(val)
     super(val.gsub(/[\r\n\f]/, "").strip)
   end
