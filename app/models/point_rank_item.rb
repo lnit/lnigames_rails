4c82d5c064ac0ea3ca384e928b3dc768c418ab6f
lnit <6626484+lnit@users.noreply.github.com>
2025-02-27 01:51:35 +0900

    今後時刻をスコアにすることを見据えてスコア保存用カラムをfloatに変更

--- app/models/point_rank_item.rb
+++ app/models/point_rank_item.rb
@@ -1,12 +1,3 @@
-class PointRankItem < ApplicationRecord
+class PointRankItem < RankItem
   include Decorator::PointScore
-
-  belongs_to :ranking_board
-
-  attribute :name, :string,  default: "No Name"
-
-  def name=(val)
-    val = val.to_s
-    super(val.gsub(/[\r\n\f]/, "").strip)
-  end
 end
